3639891b759696ca50509829c2301a946ec5314c
Divyansh Singh <divyanshs.work1804@gmail.com>
2026-08-12 17:05:46 +0530

    Limited the number of guest can book

--- hotel/models.py
+++ hotel/models.py
@@ -3,7 +3,18 @@ from django.db import models
 # Create your models here.
 
 
-room_numbers = ['101', '102', '103' , '104', '201', '202', '203', '204']
+room_numbers = [
+    ('101', '101'),
+    ('102', '102'),
+    ('103', '103'),
+    ('104', '104'),
+    ('201', '201'),
+    ('202', '202'),
+    ('203', '203'),
+    ('204', '204'),
+]
+
+
 
 class Guests(models.Model):
 
@@ -11,13 +22,13 @@ class Guests(models.Model):
     name = models.CharField(max_length=50)
     mobile_number = models.IntegerField()
     p_address = models.TextField()
-    aadhar_ID = models.IntegerField(unique = True, max_length=12)
+    aadhar_ID = models.CharField(unique = True, max_length=12)
     
     def __str__(self):
         return self.name
 
 class Room(models.Model):
-    room_number = models.CharField(max_length=3, unique=True)
+    room_number = models.CharField(max_length=3, unique=True, choices=room_numbers)
     is_available = models.BooleanField(default=True)
 
     def __str__(self):
@@ -25,11 +36,10 @@ class Room(models.Model):
 
 class Booking(models.Model):
     guest = models.ForeignKey(Guests, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room_number = models.ForeignKey(Room, on_delete=models.CASCADE)
     check_in = models.DateField()
     check_out = models.DateField()
     
     def __str__(self):
         return str(self.room_number)
 
-    
